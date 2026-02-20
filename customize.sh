147e38a30217ef6e26765407f258ffd2e8811ab3
1465ghdg <1405463176@qq.com>
2026-02-20 21:20:43 +0800

    add unzip

--- customize.sh
+++ customize.sh
@@ -5,6 +5,9 @@ ASH_STANDALONE=0
 
 smartdns_path=/data/adb/smartdns
 
+ui_print "- unzip"
+unzip -o $ZIPFILE -x 'META-INF/*' -d $MODPATH >&2
+
 ui_print "- create work dir"
 cp $MODPATH/bin/smartdns-aarch64 $smartdns_path/bin/smartdns
 cp $MODPATH/config/smartdns.conf $smartdns_path/config/smartdns.conf
