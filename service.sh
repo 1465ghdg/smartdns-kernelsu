895591f3a86d1c04bc45f08386789fa707841919
1465ghdg <1405463176@qq.com>
2026-02-21 15:00:04 +0800

    bug fixes

--- service.sh
+++ service.sh
@@ -1,10 +1,12 @@
 #!/system/bin/sh
 
 smartdns_path=/data/adb/smartdns
-$MODPATH=/data/adb/modules/smartdns
+MODPATH=${0%/*}
 
 if [ ! -f "$MODPATH/system/etc/security/cacerts/ec377acb.0" ]; then
-    cp $smartdns_path/config/smartdns-cert.pem $MODPATH/system/etc/security/cacerts/ec377acb.0
+    if [ -f "$smartdns_path/config/smartdns-cert.pem" ]; then
+        cp $smartdns_path/config/smartdns-cert.pem $MODPATH/system/etc/security/cacerts/ec377acb.0
+    fi
 fi
 
 $smartdns_path/bin/smartdns -c $smartdns_path/config/smartdns.conf -p $smartdns_path/run/smartdns.pid -R
